47f09b4ae3a5bf75b83ba01a8a7d64bf5769d5d1
Niles87 <nbing19@gmail.com>
2021-03-28 20:18:44 -0700

    added schema with typedefs and resolver

---
+++ schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    thoughts(username: String): [Thought]
+  }
+`;
+
+module.exports = typeDefs;
