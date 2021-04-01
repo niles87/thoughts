c425a70596053a0e7b790a9d591090f957fc4b53
Niles87 <nbing19@gmail.com>
2021-04-01 05:54:49 -0700

    added dynamic content to profile page and single thought

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_USER } from '../utils/queries';
+import ThoughtList from '../components/ThoughtList';
+import FriendsList from '../components/FriendsList';
 
 const Profile = () => {
+  const { username: userParam } = useParams();
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { username: userParam },
+  });
+  const user = data?.user || {};
+
+  if (loading) return <div>Loading...</div>;
   return (
     <div>
-      <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          {/* Viewing <usernames>'s profile. */}
+      <div className='flex-row mb-3'>
+        <h2 className='bg-dark text-secondary p-3 display-inline-block'>
+          Viewing {user.username}'s profile.
         </h2>
       </div>
 
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">{/* PRINT THOUGHT LIST  */}</div>
+      <div className='flex-row justify-space-between mb-3'>
+        <div className='col-12 mb-3 col-lg-8'>
+          <ThoughtList
+            title={`${user.username}'s thoughts...`}
+            thoughts={user.thoughts}
+          />
+        </div>
 
-        <div className="col-12 col-lg-3 mb-3">{/* PRINT FRIEND LIST */}</div>
+        <div className='col-12 col-lg-3 mb-3'>
+          <FriendsList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
+        </div>
       </div>
     </div>
   );
